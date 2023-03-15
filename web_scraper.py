da3f055d62bd6a1cb2776c278fbca692446ab8f5
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-15 23:42:59 +0100

    added datetime functionality

--- web_scraper.py
+++ web_scraper.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from datetime import datetime
 
 all_numbers = []
 years = [i for i in range(1955, 2023 + 1)]
@@ -14,17 +13,15 @@ for number in years:
 
     for drawing in all_drawings:
         date = drawing.find('time', class_='zahlensuche_datum').text.strip()
-
-        date = datetime.strptime(date, '%d.%m.%y')
-
         numbers = drawing.find_all('div', class_='zahlensuche_zahl')
         numbers = [int(number.text) for number in numbers]
-        super_number = int(drawing.find('div', class_='zahlensuche_zz').text)
+        super_number = drawing.find('div', class_='zahlensuche_zz').text
+        if super_number == '':
+            super_number = -1
         data = [date, *numbers, super_number]
         all_numbers.append(data)
-        break
 
 
 df = pd.DataFrame(all_numbers, columns=['date', 'number_1', 'number_2', 'number_3', 'number_4', 'number_5', 'number_6', 'super_number'])
-
+df['date'] = pd.to_datetime(df['date'], format='%d.%m.%Y')
 df.to_csv('lotto_numbers.csv', index=False)
