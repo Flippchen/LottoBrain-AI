8caeb530b10aa4a8ebbced3f68c01fee8ea3977e
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-18 15:01:09 +0100

    added comments

--- statistical_approach.py
+++ statistical_approach.py
@@ -1,22 +1,30 @@
+# Description: This script uses a statistical approach to predict the next winning numbers.
+
+# Import libraries
 from analysis.utils import number_frequency
 import pandas as pd
 import numpy as np
 
 
-def calculate_probabilities(frequency):
+# Calculate the probabilities of each number
+def calculate_probabilities(frequency: pd.Series) -> pd.Series:
     total_draws = frequency.sum()
     probabilities = frequency / total_draws
     return probabilities
 
 
-data = pd.read_csv('lotto_numbers.csv')
-calc_frequency = number_frequency(data)
-calc_probabilities = calculate_probabilities(calc_frequency)
-
-
-def generate_numbers(probabilities, n=6):
+# Generate the next lotto numbers
+def generate_numbers(probabilities: pd.Series, n: int = 6) -> np.ndarray:
     return np.random.choice(probabilities.index, size=n, replace=False, p=probabilities.values)
 
 
+# Load the data from csv file
+data: pd.DataFrame = pd.read_csv('lotto_numbers.csv')
+# Calculate the frequency of each number
+calc_frequency = number_frequency(data)
+# Calculate the probabilities of each number
+calc_probabilities = calculate_probabilities(calc_frequency)
+
+# Generate the next lotto numbers
 predicted_numbers = generate_numbers(calc_probabilities)
 print("Generated numbers based on historical frequency:", predicted_numbers)
