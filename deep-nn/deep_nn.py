273ed7a673737bdfd8f8dfd184c3b9e253997a88
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-19 00:50:34 +0100

    added keras model for all numbers

--- deep-nn/deep_nn.py
+++ deep-nn/deep_nn.py
@@ -4,11 +4,13 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
 import pandas as pd
+import numpy as np
 
 # Load data
 data = pd.read_csv('../lotto_numbers.csv')
 number_columns = ['number_1', 'number_2', 'number_3', 'number_4', 'number_5', 'number_6']
 
+# Predict super number with Keras
 # Split the data into input features (X) and target variable (y)
 X = data[number_columns]
 y = data['super_number']
@@ -21,7 +23,7 @@ def create_keras_model():
     model = Sequential()
     model.add(Dense(64, input_dim=6, activation='relu'))
     model.add(Dense(32, activation='relu'))
-    model.add(Dense(6))  # Modify the output layer to have 6 output units
+    model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
 
@@ -35,8 +37,7 @@ keras_predictions = keras_model.predict(X_test)
 keras_mse = mean_squared_error(y_test, keras_predictions)
 print("KerasRegressor Mean Squared Error:", keras_mse)
 
-
-# Predict all numbers with XGBoost
+# Predict all numbers with Keras
 X = data[['date']]
 y = data.drop(columns=['date'])
 
@@ -46,3 +47,27 @@ X['day'] = X['date'].str[8:10].astype(int)
 X = X.drop(columns=['date'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+def create_keras_model():
+    model = Sequential()
+    model.add(Dense(64, input_dim=3, activation='relu'))  # Update input_dim to 3
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(7))  # Modify the output layer to have 7 output units
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
+
+
+keras_model = KerasRegressor(build_fn=create_keras_model, epochs=100, batch_size=16, verbose=0)
+keras_model.fit(X_train, y_train)
+
+# Make predictions and calculate the mean squared error
+keras_predictions = keras_model.predict(X_test)
+
+keras_mse = mean_squared_error(y_test, keras_predictions, multioutput='raw_values')
+print("KerasRegressor Mean Squared Error for each number:", keras_mse)
+
+keras_predictions_rounded = np.round(keras_predictions).astype(int)
+print("KerasRegressor Predicted Numbers:\n", keras_predictions_rounded[0])
+
+
